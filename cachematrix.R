9bd0f8c87c0fb9dd889dd01a150b097f1c629eb4
Alex Nunes <alexjrns@users.noreply.github.com>
2019-12-16 22:41:25 -0300

    Criadas as funções

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  #limpa o antigo valor de cache
+  mat_inv <- NULL
+  
+  set <- function(y){
+    #setando o valor
+    x <<- y 
+    
+    #limpando o cache
+    mat_inv <<- NULL 
+  }
+  
+  #obtem o valor da matriz
+  get <- function() x
+  
+  #quando não há cache
+  setInversa <- function(inversa) mat_inv <<- inversa
+  
+  #função que inverte
+  getInversa <- function() mat_inv
+  
+  #retorna uma lista com as funções
+  list(set = set, 
+       get = get, 
+       setInversa = setInversa, 
+       getInversa = getInversa
+       )
 }
 
 
